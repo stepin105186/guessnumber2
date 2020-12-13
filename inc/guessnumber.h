2277df6609d01c6684a2887c2c96fd8a32ff8a18
stepin105186 <69074362+stepin105186@users.noreply.github.com>
2020-12-13 13:40:45 +0530

    Create guessnumber.h

---
+++ inc/guessnumber.h
@@ -0,0 +1,15 @@
+#ifndef GUESSNUMBER_H
+#define GUESSNUMBER_H
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+void MainMenu();
+int GetChoice(int min,int max);
+double guessGame( );
+void PrintScore( );
+void Quit();
+int NumDigits(int number);
+int numPoints(double timesec);
+
+#endif
